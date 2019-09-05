c36c774742faed71cebcba9bae24216c70b28646
super-programmer <zkp593655843@163.com>
2019-09-05 23:03:27 +0800

    feat

--- pages/question/list/index.js
+++ pages/question/list/index.js
@@ -13,11 +13,17 @@ Page({
    */
   handleTabChange(data) {
     const index = data.detail.index;
- 
     if (index === 1) {
       wx.navigateTo({
         url: router.profile
       });
     }
+  },
+
+  toCreatQuestion() {
+    console.log("ddd")
+    wx.navigateTo({
+      url: router.questionCreate
+    });
   }
 })
